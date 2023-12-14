1c7129cd62121dd8ae9c7be119aca3e03366ab7b
0xrin1 <0xrin1@protonmail.com>
2023-12-14 01:34:14 +0100

    add ABIS for joe router and factory

--- build/app.js
+++ build/app.js
@@ -1,5 +1,7 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+import routerABI from './abis/router.json' assert { type: 'json' };
+import factoryABI from './abis/factory.json' assert { type: 'json' };
+console.log('routerABI', routerABI);
+console.log('factoryABI', factoryABI);
 const initMessage = 'Joe Pair Listener started';
 console.log(initMessage);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,9 @@
+import routerABI from './abis/router.json' assert { type: 'json' };
+import factoryABI from './abis/factory.json' assert { type: 'json' };
+
+console.log('routerABI', routerABI);
+console.log('factoryABI', factoryABI);
+
 const initMessage: string = 'Joe Pair Listener started';
 
 console.log(initMessage);
