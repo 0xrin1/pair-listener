082ae737c10bb0d4a445342c20fea62b1ba6fdf3
0xrin1 <0xrin1@protonmail.com>
2023-12-14 01:35:18 +0100

    remove committed build files

--- build/app.js
+++
@@ -1,7 +0,0 @@
-import routerABI from './abis/router.json' assert { type: 'json' };
-import factoryABI from './abis/factory.json' assert { type: 'json' };
-console.log('routerABI', routerABI);
-console.log('factoryABI', factoryABI);
-const initMessage = 'Joe Pair Listener started';
-console.log(initMessage);
-//# sourceMappingURL=app.js.map
