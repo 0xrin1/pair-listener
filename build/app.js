506b0010ae0861f7b838859a57ed02ae6b902fbd
0xrin1 <0xrin1@protonmail.com>
2023-12-14 01:27:09 +0100

    init typescript

---
+++ build/app.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const initMessage = 'Joe Pair Listener started';
+console.log(initMessage);
+//# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,3 @@
+const initMessage: string = 'Joe Pair Listener started';
+
+console.log(initMessage);
